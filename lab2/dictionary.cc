9d43dce3a6909757de3474fb8b317490fe3b33fd
Axel Müller <axel.muller.2685@student.lu.se>
2022-02-07 17:47:29 +0100

    add3d

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -11,21 +11,11 @@ using std::string;
 using std::unordered_set;
 using std::vector;
 using namespace std;
-<<<<<<< HEAD
 
 Dictionary::Dictionary() {
 	unordered_set<string> word_set;
 	ifstream infile("new.txt");
 	
-=======
-
-Dictionary::Dictionary()
-
-{
-	words_set = unordered_set<string>();
-	ifstream infile("words.txt");
-
->>>>>>> 9f2a318c94b195ab11e4545b3a4c628e058681c5
 }
 	
 
